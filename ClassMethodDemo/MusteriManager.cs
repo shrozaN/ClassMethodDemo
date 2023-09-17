a23ed9e7a9f347ed90f8ca3b59e15ace7c29cb64
Tufan Ozan Karahan <tufanozan1@outlook.com>
2023-09-17 18:11:39 +0300

    Add project files.

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri) 
+        {
+            Console.WriteLine("Başarıyla eklenen Müşteri = " + musteri.Ad + " " + musteri.Soyad + ": " + musteri.Yas + " Yaş" + " Id: " + musteri.Id);
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Başarıyla silinen Müşteri = " + musteri.Ad + " " + musteri.Soyad + ": " + musteri.Yas + " Yaş" + " Id: " + musteri.Id);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using ClassMethodDemo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Musteri musteri1 = new Musteri();
+        musteri1.Ad = "ABC";
+        musteri1.Soyad = "CBA";
+        musteri1.Yas = 48;
+        musteri1.Id = 1;
+
+        Musteri musteri2 = new Musteri();
+        musteri2.Ad = "XYZ";
+        musteri2.Soyad = "ZYX";
+        musteri2.Yas = 36;
+        musteri2.Id = 2;
+
+        Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+        Console.WriteLine("Listeleme İşlemi");
+
+        foreach (Musteri musteri in musteriler)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + ": " + musteri.Yas + " Yaş" + " " + "Id: "  + musteri.Id);
+        }
+
+        Console.WriteLine("------------------------");
+        
+        Console.WriteLine("Ekleme İşlemi");
+        MusteriManager musteriManager = new MusteriManager();
+        musteriManager.MusteriEkle(musteri1);
+
+        Console.WriteLine("------------------------");
+
+        Console.WriteLine("Silme İşlemi");
+        MusteriManager musteriManager1 = new MusteriManager();
+        musteriManager1.MusteriSil(musteri2);
+
+    }
+}
